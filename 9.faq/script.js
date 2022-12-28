8b2b8cd8cf6d76f28fee67b95cb723f203176f0d
Tejas Dewett <91184807+TejasDewett@users.noreply.github.com>
2022-12-28 12:14:14 +0530

    Delete 9.faq directory

--- 9.faq/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelectorAll('.faq-toggle')
-
-btn.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
